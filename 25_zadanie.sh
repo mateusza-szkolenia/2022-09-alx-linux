e0e251a987ccefe3e61def6d46964e339879be0c
Mateusz Adamowski <m.adamowski@alx.pl>
2022-09-29 16:08:22 +0200

    skrypt robiący kopię /etc

--- 25_zadanie.sh
+++ 25_zadanie.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 dzisiaj=$(date +%Y%m%d)
+zrodlo="/etc"
 cel="/tmp/etc-$dzisiaj"
 
 echo "Data dzisiejsza: $dzisiaj"
 echo "Katalog z backupem: $cel"
+
+if test -d "$cel"
+then
+	echo "Katalog istnieje, nic do roboty"
+else
+	cp -r "$zrodlo" "$cel"
+fi
+
