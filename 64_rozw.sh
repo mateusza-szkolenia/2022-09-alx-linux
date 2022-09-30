9a0a4af4352161b2e1f3810f1a6b06eb5e767fc2
Mateusz Adamowski <m.adamowski@alx.pl>
2022-09-30 14:22:19 +0200

    hashe md5

--- 64_rozw.sh
+++ 64_rozw.sh
@@ -5,8 +5,9 @@ do
 	if grep -q -i "ala" "$skrypt"
 	then
 		linie="$(wc -l < "$skrypt")"
+		md5h="$(md5sum < "$skrypt" | tr -d '-' )"
 
-		echo "$skrypt ($linie)"
+		echo "$skrypt ($linie) $md5h"
 	fi
 
 done
