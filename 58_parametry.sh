8e8036717c6c52b2123af913d205cc49e3408518
Mateusz Adamowski <m.adamowski@alx.pl>
2022-09-30 13:38:54 +0200

    kopia

---
+++ 58_parametry.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+function powitanie {
+	echo "-----powitanie------"
+	echo "p1 = $1"
+	echo "p2 = $2"
+	echo "p3 = $3"
+	echo "wszystkie parametry: [$@]"
+}
+
+powitanie Mateusz
+
+powitanie Mateusz ALX Linux
+
+powitanie a b c d e f g
