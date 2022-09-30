b4d3d596af598bec2ad809c650fe4725ab98f515
Mateusz Adamowski <m.adamowski@alx.pl>
2022-09-30 13:38:12 +0200

    shift

--- 57_params.sh
+++ 57_params.sh
@@ -4,10 +4,20 @@ function abc {
 
 	echo "parametr 1: $1"
 	echo "parametr 2: $2"
+	echo "ile: $#"
+	echo "wszystkie: $@"
+
+	shift
+
+	echo "TU BYŁ SHIFT"
+
+	echo "parametr 1: $1"
+	echo "parametr 2: $2"
+	echo "ile: $#"
 	echo "wszystkie: $@"
 
 }
 
 
 
-abc Ala Basia Czesia Dorota Ewa Frania Grażyna Halina Iwona
+abc Ala Basia Czesia Dorota Ewa Frania Grażyna Halina Iwona Julia Karolina Lucyna Łucja Monika Natalina
