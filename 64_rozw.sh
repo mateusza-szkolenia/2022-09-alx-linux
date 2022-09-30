7773d3a8b797a1363eb87aefc3736053474d33b5
Mateusz Adamowski <m.adamowski@alx.pl>
2022-09-30 14:26:38 +0200

    pozbywam się if, przeskakuję niepasujące pliki używając continue

--- 64_rozw.sh
+++ 64_rozw.sh
@@ -4,15 +4,14 @@
 
 for skrypt in *.sh
 do
-	if grep -q -i "ala" "$skrypt"
-	then
-		linie="$(wc -l < "$skrypt")"
-		md5h="$(md5sum < "$skrypt" | tr -d '-' )"
+	grep -q -i "ala" "$skrypt" || continue
 
-		(( razem += linie ))
+	linie="$(wc -l < "$skrypt")"
+	md5h="$(md5sum < "$skrypt" | tr -d '-' )"
 
-		echo "$skrypt ($linie) $md5h"
-	fi
+	(( razem += linie ))
+
+	echo "$skrypt ($linie) $md5h"
 done
 
 echo "Razem linii: $razem"
