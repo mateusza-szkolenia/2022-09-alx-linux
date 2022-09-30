e8c7e899cb011a4638056f9ae886fc0d8a3519a7
Mateusz Adamowski <m.adamowski@alx.pl>
2022-09-30 11:52:34 +0200

    funkcja

--- 52_funk.sh
+++ 52_funk.sh
@@ -4,7 +4,14 @@ function powitanie {
 	echo "Dzień dobry, dzisiaj jest $(date +%F)"
 }
 
+pozegnanie () {
+	echo "Bye"
+}
+
 whoami
 powitanie
 whoami
-
+pozegnanie
+pozegnanie
+pozegnanie
+pozegnanie
