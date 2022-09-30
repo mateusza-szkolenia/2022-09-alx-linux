c1b982fafc5b7f629e02b6b28811e9382bdd9462
Mateusz Adamowski <m.adamowski@alx.pl>
2022-09-30 13:49:34 +0200

    krócej

--- 60_petle.sh
+++ 60_petle.sh
@@ -3,10 +3,8 @@
 for plik in raport-{2020..2022}-{01..12}.txt
 do
 	echo "plik: $plik"
-	if [ -e "$plik" ]; then
-		echo "Plik istnieje"
-	else
-		echo "Plik nie istnieje"
-	fi
+
+	[ -e "$plik" ] && echo "Istnieje" || echo "Nie istnieje"
+
 done
 
