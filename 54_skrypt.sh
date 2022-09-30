73dc219a3f796abd1946111b7b532d3fb73aca4a
Mateusz Adamowski <m.adamowski@alx.pl>
2022-09-30 12:11:38 +0200

    source

---
+++ 54_skrypt.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source 54_ustawienia.sh
+
+echo "Witaj w $firma"
+echo "Jesteśmy na szkoleniu: $szkolenie"
+echo "Adres repozytorium to: $repo"
+
+
---
+++ 54_ustawienia.sh
@@ -0,0 +1,3 @@
+firma=ALX
+szkolenie=Linux
+repo=https://github.com/mateusza-szkolenia/2022-09-alx-linux
